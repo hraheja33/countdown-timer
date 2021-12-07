fb3ef02a2d958475be7fff3d7916429315907a66
hraheja33 <89431410+hraheja33@users.noreply.github.com>
2021-12-07 10:49:21 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+console.log(items);
+
+let futureDate = new Date(2022,0,24,11,30, 00);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const seconds = futureDate.getSeconds();
+const month = months[futureDate.getMonth()];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes} am`;
+
+const futureTime = futureDate.getTime();
+
+const getRemaindingTime = () => {
+  const today = new Date().getTime();
+
+  const t = futureTime - today;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  const values = [days, hours, minutes, seconds];
+  // console.log(values);
+
+  const format = item => {
+    if(item < 10){
+      return(item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  });
+
+  if(t < 0 ){
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+  }
+}
+ 
+let countdown = setInterval(getRemaindingTime,1000);
+
+getRemaindingTime();
